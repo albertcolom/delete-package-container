f079ae5127955a3ac7b5d6492802a86942eae2ae
Albert Colom <albert.colom@stayforlong.com>
2022-10-06 23:51:22 +0200

    Implement remove package  by version

--- index.js
+++ index.js
@@ -6,22 +6,26 @@ async function run() {
     const octokit = github.getOctokit(token);
 
     const pkg = core.getInput('package').split(':');
-    const packageName = pkg[0] || null;
+    const packageName = pkg[0] ? pkg[0].replace('/', '%2F') : null;
     const packageTag = pkg[1] || null;
 
     if (null === packageName || null == packageTag) {
-        core.setFailed(`The package name "${pkg} is invalid. Example format: project-name/package:tag`);
+        core.setFailed(`The package name "${pkg} is invalid. Example format: owner/image_name:tag`);
     }
 
     const packages = await octokit.rest.packages.getAllPackageVersionsForPackageOwnedByAuthenticatedUser({
         package_type: 'container',
-        package_name: packageName.replace('/', '%2F'),
+        package_name: packageName,
     });
 
     Object.values(packages.data).forEach(pkg => {
-        console.log('Package ID: '+ pkg.id)
-        console.log(pkg.metadata.container.tags)
-        console.log(pkg.metadata.container.tags.includes(packageTag))
+        if (pkg.metadata.container.tags.includes(packageTag)) {
+            octokit.rest.packages.deletePackageVersionForAuthenticatedUser({
+                package_type: 'container',
+                package_name: packageName,
+                package_version_id: pkg.id,
+            });
+        }
     });
 }
 
